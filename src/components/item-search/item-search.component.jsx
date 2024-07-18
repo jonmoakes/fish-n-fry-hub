40b61a567047b45822ef09e1c5d7308e728462c2
Jonathan Oakes <jonathanmartynoakes@gmail.com>
2024-07-18 14:53:10 +0100

    added balancer to search text

--- src/components/item-search/item-search.component.jsx
+++ src/components/item-search/item-search.component.jsx
@@ -1,3 +1,4 @@
+import Balancer from "react-wrap-balancer";
 import { ParentDiv } from "../../styles/div/div.styles";
 import { Text } from "../../styles/p/p.styles";
 import SearchBar from "../searchbar/searchbar.component";
@@ -10,9 +11,13 @@ const ItemSearch = ({
   return (
     <ParentDiv>
       {!searchField.length ? (
-        <Text>search for products by typing in a query below:</Text>
+        <Text>
+          <Balancer>search for products by typing in a query below:</Balancer>
+        </Text>
       ) : (
-        <Text>tap the red 'x' button to clear the search field</Text>
+        <Text>
+          <Balancer>tap the red 'x' button to clear the search field</Balancer>
+        </Text>
       )}
       <SearchBar
         {...{ handleSearchFieldChange, resetSearchField, searchField }}
