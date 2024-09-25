bdb4fec09a5ca52778673a06a376c4b106096a86
Jonathan <jonathanmartynoakes@gmail.com>
2024-09-25 10:54:28 +0100

    added more routes to nav warning

--- src/hooks/use-is-route-with-nav-warning.js
+++ src/hooks/use-is-route-with-nav-warning.js
@@ -1,13 +1,21 @@
 import { useLocation } from "react-router-dom";
 
-import { chooseOptionsRoute } from "../strings/routes/routes-strings";
+import {
+  checkoutRoute,
+  chooseOptionsRoute,
+  repeatOrderRoute,
+} from "../strings/routes/routes-strings";
 
 const useIsRouteWithNavWarning = () => {
   const location = useLocation();
   const path = location.pathname;
 
   const isRouteWithNavWarning = () => {
-    return path === chooseOptionsRoute ? true : false;
+    return path === chooseOptionsRoute ||
+      path === checkoutRoute ||
+      path === repeatOrderRoute
+      ? true
+      : false;
   };
 
   return { isRouteWithNavWarning };
